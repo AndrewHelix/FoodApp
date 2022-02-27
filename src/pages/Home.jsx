efbe8a5205ffca60b288bd75162819ef6dce242c
AndrewHelix <andreyshulga95@gmail.com>
2022-02-27 18:48:42 +0300

    feat: add search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,52 @@
-import { useState, useEffect } from "react"
-import { getAllCategories } from "../api"
-import {Preloader} from '../components/Preloader'
-import { CategoryList } from "../components/CategoryList"
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getAllCategories } from "../api";
+import { Preloader } from "../components/Preloader";
+import { CategoryList } from "../components/CategoryList";
+import { Search } from "../components/Search";
 
 function Home() {
-  const [catalog, setCategories] = useState([])
+  const [catalog, setCategories] = useState([]);
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const { pathname, search } = useLocation();
+  const navigate = useNavigate();
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.strCategory.toLowerCase().includes(str.toLowerCase())
+      )
+    );
+    navigate({
+      pathname,
+      search: `?search=${str}`,
+    });
+  };
 
   useEffect(() => {
-    getAllCategories().then(data => {
-      setCategories(data.categories)
-    })
-  }, [])
+    getAllCategories().then((data) => {
+      setCategories(data.categories);
+      setFilteredCatalog(search ?
+        data.categories.filter((item) =>
+          item.strCategory
+            .toLowerCase()
+            .includes(search.split("=")[1].toLowerCase())
+        ) : data.categories
+      );
+    });
+  }, [search]);
 
-  
-  return <>
-    {!catalog.length ? <Preloader /> : (<CategoryList catalog={catalog} />)}
-  </>
+  return (
+    <>
+      <Search cb={handleSearch} />
+      {!catalog.length ? (
+        <Preloader />
+      ) : (
+        <CategoryList catalog={filteredCatalog} />
+      )}
+    </>
+  );
 }
 
-export {Home}
+export { Home };
